8603f041e40b5e05bb6f4abb9df1a14c6ac676fa
NickProvidokhin <nickprovidohin@gmail.com>
2024-10-26 12:13:35 +0300

    add space and colon

--- src/main/java/ru/providokhin/loggingstarter/LoggingStarterAutoConfiguration.java
+++ src/main/java/ru/providokhin/loggingstarter/LoggingStarterAutoConfiguration.java
@@ -2,6 +2,6 @@ package ru.providokhin.loggingstarter;
 
 public class LoggingStarterAutoConfiguration {
     public static void println(String str) {
-        System.out.println("Вызвано из maven библиотеки" + str);
+        System.out.println("Вызвано из maven библиотеки: " + str);
     }
 }
